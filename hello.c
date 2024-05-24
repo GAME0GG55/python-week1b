2c5fb895f0b50f86ba5a81575c3dd6a3a27f293e
GAME0GG55 <itthekhnoloyisarsnthes92@gmail.com>
2024-05-24 16:06:15 +0700

    GAME0GG55

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 15;
+    printf("hello world!\n")
+    printf("my name is GAME\n");
+    PRINTF("Age: %d\n',age");
+    return 0;
+}
